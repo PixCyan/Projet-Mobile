8f4f6a52d26a21b2b2ecf6a62bbcd6f07b0380c0
PixCyan <anaisraffenne@gmail.com>
2015-06-03 21:53:47 +0200

    Maj Multiplication

--- app/src/main/java/com/example/raffennn/raffennn/Multiplication.java
+++ app/src/main/java/com/example/raffennn/raffennn/Multiplication.java
@@ -1,53 +1,42 @@
 package com.example.raffennn.raffennn;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import java.util.Random;
 
 
 public class Multiplication extends ActionBarActivity {
+    public static final String FINAL_SCORE = "0";
+    public final static int MULT_REQUEST = 0;
     private static final Random random = new Random();
+    private int nbCalc = 0;
     private int nb1;
     private int nb2;
+    private Calculs c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplication);
 
-        //TODO affichage random
         nb1 = random.nextInt(10);
         nb2 = random.nextInt(10);
-        Calculs c = new Calculs(nb1, nb2);
+        c = new Calculs(nb1, nb2);
+
+        TextView view1 = (TextView)findViewById(R.id.chiffre1);
+        view1.setText(nb1 + " x " + nb2 + " = ");
 
 
-        //TODO afficher text view pour table
-        ActionBar.LayoutParams params = new ActionBar.LayoutParams(50,100);
-        LinearLayout layout = (LinearLayout) findViewById(R.id.mult);
-        TextView table = new TextView(this);
-        table.setText(nb1 + " x " + nb2 + " = ");
-        table.setWidth(50);
-        table.setHeight(50);
-
-        EditText rep = new EditText(this);
-        rep.setText(" ? ");
-        rep.setLayoutParams(params);
-        rep.setTextSize(10);
-        layout.addView(table);
-        layout.addView(rep);
 
 
-        /*for(int i = 0; i < 3; i++) {
+        /*
+        //Création dynamique
+        for(int i = 0; i < 3; i++) {
             TextView table = new TextView(this);
             table.setText(nb1 + " x " + nb2 + " = ");
             table.setWidth(50);
@@ -59,8 +48,7 @@ public class Multiplication extends ActionBarActivity {
             rep.setTextSize(10);
             layout.addView(table);
             layout.addView(rep);
-        }*/
-
+        }
         Button boutonAide = new Button(this);
         boutonAide.setText("Quitter");
         boutonAide.setOnClickListener(new View.OnClickListener() {
@@ -69,7 +57,6 @@ public class Multiplication extends ActionBarActivity {
                 aide();
             }
         });
-
         Button boutonMenu = new Button(this);
         boutonMenu.setText("Quitter");
         boutonMenu.setOnClickListener(new View.OnClickListener() {
@@ -78,9 +65,8 @@ public class Multiplication extends ActionBarActivity {
                 quitter();
             }
         });
-
         layout.addView(boutonAide);
-        layout.addView(boutonMenu);
+        layout.addView(boutonMenu);*/
     }
 
 
@@ -106,18 +92,33 @@ public class Multiplication extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //TODO valider
+    //TODO valider à revoir
     public void valider(View view) {
-
+        TextView view2 = (TextView)findViewById(R.id.rep);
+        int res = Integer.parseInt((String)view2.getText());
+        int res2 = c.caclculMult();
+        if(res == res2) {
+            c.scrorePlus();
+        }
+        /*if(nbCalc != 3) {
+            Intent intent = new Intent(this, Multiplication.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else {*/
+            Intent intent = new Intent(this, Score.class);
+            int score = c.getScoreFinal();
+            intent.putExtra(FINAL_SCORE, score);
+            startActivityForResult(intent, MULT_REQUEST);
+        //}
     }
 
 
     //TODO aide
-   public void aide() {
+   public void aide(View view) {
 
    }
 
-    private void quitter() {
+    public void quitter(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
